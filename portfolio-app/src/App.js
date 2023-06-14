423e7ce7575e68b8dbe82722ae0538aad1e3ea85
Bianca Aspin <bianca@mj94.net>
2023-06-13 18:03:09 -0700

    Add dividers and Skills panel.

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {
   ChakraProvider,
   theme,
+  Divider
 } from '@chakra-ui/react';
 import Header from './components/Header';
 import About from './components/About';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Header />
+      <Divider colorScheme="purple"/>
       <About />
+      <Divider />
+      <Skills />
     </ChakraProvider>
   );
 }
