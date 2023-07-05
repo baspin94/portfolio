468e01ddaff8d19a9e5d759eaddab4b6d974e077
Bianca Aspin <bianca@mj94.net>
2023-07-05 16:44:45 -0700

    Display interactive project cards which can be tapped to show/hide additional information.

--- portfolio-app/src/components/ProjectCard.js
+++ portfolio-app/src/components/ProjectCard.js
@@ -1,7 +1,42 @@
-function ProjectCard({ name }){
+import { Card, CardBody, CardHeader, CardFooter, Image, Heading, Text, Link, Button, HStack } from "@chakra-ui/react";
+import { useState } from "react";
+
+function ProjectCard({ details }){
+    const [showDetails, setShowDetails] = useState(false)
+
+    function handleClick(e) {
+        if (e.target.type !== "button") setShowDetails(!showDetails)
+    }
+
+    
     return(
-        <p>{name}</p>
+        <Card size="sm" border="1px" align="center" onClick={handleClick}>
+            <CardHeader align="center" >
+                <Heading as="h3" size="sm">{details.title}</Heading>
+            </CardHeader>
+            <CardBody>
+                { showDetails
+                    ? <Text>{details.description}</Text> 
+                    : <Image src={details.image} alt={details.alt} w="100%" h="100%"/>
+                }
+            </CardBody>
+            <CardFooter>
+                <HStack>
+                    <Link href={details.app} target="_blank" rel="noopener noreferrer">
+                        <Button>App</Button>
+                    </Link>
+                    <Link href={details.repo} target="_blank" rel="noopener noreferrer">
+                        <Button>Repo</Button>
+                    </Link>
+                    <Link href={details.demo} target="_blank" rel="noopener noreferrer">
+                        <Button>Demo</Button>
+                    </Link>
+                </HStack>
+            </CardFooter>
+        </Card>
     )
+
+    
 }
 
 export default ProjectCard;
--- portfolio-app/src/components/Projects.js
+++ portfolio-app/src/components/Projects.js
@@ -1,17 +1,19 @@
-import { Heading, Box } from "@chakra-ui/react";
+import { Heading, Box, SimpleGrid } from "@chakra-ui/react";
 import data from "../data/projects.json"
 import ProjectCard from "./ProjectCard";
 
 function Projects() {
 
     const projectList = data.projects.map(project => {
-        return <ProjectCard key={project.id} name={project.title} />
+        return <ProjectCard key={project.id} details={project}/>
     })
 
     return (
         <Box>
             <Heading>Projects</Heading>
-            {projectList}
+            <SimpleGrid columns={3} spacing={2} margin="auto" w="80%" padding="20px">
+                {projectList}
+            </SimpleGrid>
         </Box>
     )
 }
